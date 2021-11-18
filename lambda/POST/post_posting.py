11251d48e38c88e58dc36a379b4c048ffaf18cb1
cta70 <calvin_truong@sfu.ca>
2021-11-18 01:10:45 -0800

    Renamed postings table referenced in post_postings

--- lambda/POST/post_posting.py
+++ lambda/POST/post_posting.py
@@ -8,7 +8,7 @@ import boto3
 # create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
 # use the DynamoDB object to select our table
-table = dynamodb.Table('Postings')
+table = dynamodb.Table('Posting')
 # store the current time in a human readable format in a variable
 #now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 
