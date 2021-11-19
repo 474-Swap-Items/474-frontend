f9c6e6fc8d44b10b8c522a29f879ac40d64c7eb6
jyw30 <72828118+jyw30@users.noreply.github.com>
2021-11-18 23:04:54 -0800

    Update CreateUser.js

--- src/components/users/CreateUser.js
+++ src/components/users/CreateUser.js
@@ -34,7 +34,7 @@ export default function CreateUser(props) {
 
   const makeAndSend = (values) => {
     let obj = {name: values[0], username: values[1], id: uuidv4()}
-    axios.post('https://y1nkeqjzma.execute-api.us-east-1.amazonaws.com/prod', obj)
+    axios.post('https://63pqdmx904.execute-api.us-east-1.amazonaws.com/production', obj)
     .then((res) => {console.log(res)})
     .catch((err) =>{console.log(err)})
   }
