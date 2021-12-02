c239ed3a61438ab0ac339e21e35e86bb01e43281
cta70 <calvin_truong@sfu.ca>
2021-12-02 06:14:56 -0800

    Removed create user

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import CreateUser from './CreateUser'
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 import Card from '@mui/material/Card';
