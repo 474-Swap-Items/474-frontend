9d803ea0e4cf1d26c907125ecc8e453fbcc98640
jyw30@sfu.ca <jyw30@sfu.ca>
2021-12-02 21:17:18 -0800

    fix merge conflict

--- src/components/login/Signup.jsx
+++ src/components/login/Signup.jsx
@@ -39,15 +39,9 @@ export default function Signup() {
     };
 
     const makeAndSend = (values) => {
-<<<<<<< HEAD
-        let obj = { id:uuidv4(), username: values[0], email: values[1], img_url: values[3]}
-        axios.post('https://63pqdmx904.execute-api.us-east-1.amazonaws.com/production', obj)
-            .then((res) => {console.log("Created user: " + res)})
-=======
         let obj = { id:uuidv4(), username: values[0], email: values[1], img_url: values[2]}
-        axios.post('https://1as88cndmc.execute-api.us-east-1.amazonaws.com/sfu-swap-prod', obj)
+        axios.post('https://63pqdmx904.execute-api.us-east-1.amazonaws.com/production', obj)
             .then((res) => {console.log("Created dynamo user: " + res);goToLogin();})
->>>>>>> master
             .catch((err) =>{console.log(err)})
     }
 
